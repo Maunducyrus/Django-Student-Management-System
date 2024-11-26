1f0012825e241c5477e496994c2a9f13944964d9
maunducyrus123@gmail.com <maunducyrus123@gmail.com>
2024-11-26 14:40:25 +0300

    added more templates

--- application/urls.py
+++ application/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:id>', views.view_students, name='view_student'),
     path('add/', views.add, name='add'),
+    path('edit/<int:id>/', views.edit, name='edit'),
+    path('delete/<int:id>/', views.delete, name='delete'),
 ]
